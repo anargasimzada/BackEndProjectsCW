0a1a8694cdb8976924a3940fa1f477fc078e4d6c
anargasimzada <tu7g11auv@code.edu.az>
2024-05-13 00:14:28 +0400

    part

--- MultiShop/obj/Debug/net8.0/MultiShop.AssemblyInfo.cs
+++ MultiShop/obj/Debug/net8.0/MultiShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27d5a0466b87878bd7f55b76903f00e061be3c91")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
